e0dd2f49b1df4a1efa757f1ed2e127a539a3086b
meghalakhwan <meghalakhwan9@gmail.com>
2023-09-14 22:37:04 +0530

    first commit

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const bodyEl = document.querySelector("body");
 
 inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "black";
+  } else {
+    bodyEl.style.background = "white";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
